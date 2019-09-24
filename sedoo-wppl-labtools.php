8026570330b03317bc7c95d3e3d20898237d138f
Pierre VERT <vert@obs-mip.fr>
2019-09-24 18:28:05 +0200

    gutenberg related block ready

--- sedoo-wppl-labtools.php
+++ sedoo-wppl-labtools.php
@@ -7,7 +7,7 @@
  * Author URI:      https://www.sedoo.fr 
  * Text Domain:     sedoo-wppl-labtools
  * Domain Path:     /languages
- * Version:         0.1.0
+ * Version:         0.2.0
  * GitHub Plugin URI: sedoo/sedoo-wppl-labtools
  * GitHub Branch:     master
  * @package         Sedoo_Wppl_Labtools
