15006c2d617c563b664ce9fa1e7fb7b7b72c3b5f
Pierre VERT <vert@obs-mip.fr>
2019-10-08 17:08:00 +0200

    remove tpl theme

--- sedoo-wppl-labtools-display.php
+++ sedoo-wppl-labtools-display.php
@@ -145,19 +145,24 @@ function sedoo_labtools_get_associate_content($parameters, $args, $type_of_conte
     
     // The Loop
     if ( $the_query->have_posts() ) {
-
-        echo '<section><h3>'.__( $parameters['sectionTitle'], 'sedoo-wppl-labtools' ).'</h3>';
-        echo '<ul>';
+		echo '<h2>'.__( $parameters['sectionTitle'], 'sedoo-wppl-labtools' ).'</h2>';
+		echo '<section role="listNews" class="post-wrapper">';
+		if ($type_of_content !== "post") {
+		echo '<ul>';
+		}
         while ( $the_query->have_posts() ) {
 			$the_query->the_post();
 
 			if ($type_of_content == "post") {
 				
 				include ( get_template_directory() . '/template-parts/content.php');
+				
+				// include ( plugin_dir_path(__FILE__) . "/template-parts/content.php");
 			}
 			else {
-            $titleItem=mb_strimwidth(get_the_title(), 0, 65, '...');  
-            ?>
+			$titleItem=mb_strimwidth(get_the_title(), 0, 65, '...');  
+           
+			?>
                 <li>
                     <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
                         <?php echo $titleItem ?>
@@ -166,7 +171,10 @@ function sedoo_labtools_get_associate_content($parameters, $args, $type_of_conte
             <?php
 			}
         }
-        echo '</ul></section>';
+		if ($type_of_content !== "post") {
+			echo '</ul>';
+			}
+		echo '</section>';
         /* Restore original Post Data */
         wp_reset_postdata();
     } else {
@@ -174,160 +182,5 @@ function sedoo_labtools_get_associate_content($parameters, $args, $type_of_conte
     }
 }
 
-/** ----------------------------------------------------------------------------------------------
- * ADD TEMPLATE THEME FOR PAGES
- */
-
-class PageTemplater {
-
-	/**
-	 * A reference to an instance of this class.
-	 */
-	private static $instance;
-
-	/**
-	 * The array of templates that this plugin tracks.
-	 */
-	protected $templates;
-
-	/**
-	 * Returns an instance of this class. 
-	 */
-	public static function get_instance() {
-
-		if ( null == self::$instance ) {
-			self::$instance = new PageTemplater();
-		} 
-
-		return self::$instance;
-
-	} 
-
-	/**
-	 * Initializes the plugin by setting filters and administration functions.
-	 */
-	private function __construct() {
-
-		$this->templates = array();
-
-
-		// Add a filter to the attributes metabox to inject template into the cache.
-		if ( version_compare( floatval( get_bloginfo( 'version' ) ), '4.7', '<' ) ) {
-
-			// 4.6 and older
-			add_filter(
-				'page_attributes_dropdown_pages_args',
-				array( $this, 'register_project_templates' )
-			);
-
-		} else {
-
-			// Add a filter to the wp 4.7 version attributes metabox
-			add_filter(
-				'theme_page_templates', array( $this, 'add_new_template' )
-			);
-
-		}
-
-		// Add a filter to the save post to inject out template into the page cache
-		add_filter(
-			'wp_insert_post_data', 
-			array( $this, 'register_project_templates' ) 
-		);
-
-
-		// Add a filter to the template include to determine if the page has our 
-		// template assigned and return it's path
-		add_filter(
-			'template_include', 
-			array( $this, 'view_project_template') 
-		);
-
-
-		// Add your templates to this array.
-		$this->templates = array(
-			'template-theme.php' => 'Page theme',
-		);
-			
-	} 
-
-	/**
-	 * Adds our template to the page dropdown for v4.7+
-	 *
-	 */
-	public function add_new_template( $posts_templates ) {
-		$posts_templates = array_merge( $posts_templates, $this->templates );
-		return $posts_templates;
-	}
-
-	/**
-	 * Adds our template to the pages cache in order to trick WordPress
-	 * into thinking the template file exists where it doens't really exist.
-	 */
-	public function register_project_templates( $atts ) {
-
-		// Create the key used for the themes cache
-		$cache_key = 'page_templates-' . md5( get_theme_root() . '/' . get_stylesheet() );
-
-		// Retrieve the cache list. 
-		// If it doesn't exist, or it's empty prepare an array
-		$templates = wp_get_theme()->get_page_templates();
-		if ( empty( $templates ) ) {
-			$templates = array();
-		} 
-
-		// New cache, therefore remove the old one
-		wp_cache_delete( $cache_key , 'themes');
-
-		// Now add our template to the list of templates by merging our templates
-		// with the existing templates array from the cache.
-		$templates = array_merge( $templates, $this->templates );
-
-		// Add the modified cache to allow WordPress to pick it up for listing
-		// available templates
-		wp_cache_add( $cache_key, $templates, 'themes', 1800 );
-
-		return $atts;
-
-	} 
-
-	/**
-	 * Checks if the template is assigned to the page
-	 */
-	public function view_project_template( $template ) {
-		
-		// Get global post
-		global $post;
-
-		// Return template if post is empty
-		if ( ! $post ) {
-			return $template;
-		}
-
-		// Return default template if we don't have a custom one defined
-		if ( ! isset( $this->templates[get_post_meta( 
-			$post->ID, '_wp_page_template', true 
-		)] ) ) {
-			return $template;
-		} 
-
-		$file = plugin_dir_path( __FILE__ ). get_post_meta( 
-			$post->ID, '_wp_page_template', true
-		);
-
-		// Just to be safe, we check if the file exist first
-		if ( file_exists( $file ) ) {
-			return $file;
-		} else {
-			echo $file;
-		}
-
-		// Return template
-		return $template;
-
-	}
-
-} 
-add_action( 'plugins_loaded', array( 'PageTemplater', 'get_instance' ) );
 
 ?>
