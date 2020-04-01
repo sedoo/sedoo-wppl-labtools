ba105d4823a69858bd98308db70e6373601f5979
ngruwe <nicolas.gruwe@obs-mip.fr>
2020-04-01 16:33:06 +0200

    templates simplifiés (fonctions) + tag + support du sommaire

--- single-sedoo-research-team.php
+++ single-sedoo-research-team.php
@@ -25,57 +25,31 @@ while ( have_posts() ) : the_post();
    }
    $themes = get_the_terms( $post->ID, 'sedoo-theme-labo');  
 $themeSlugRewrite = "sedoo-theme-labo";
-
-
 ?>
+
+<!-- L'AFFICHAGE COMMENCE ICI -->
 <?php
-if (get_the_post_thumbnail()) {
-?>
-<header id="cover">
-   <?php the_post_thumbnail(); ?>
-</header>
-<?php 
+sedoo_labtools_test_if_post_thumbnail_and_display();
+// Show title first on mobile
+if (get_field( 'table_content' )) {
+   sedoo_labtools_display_title_on_top_on_mobile();
 }
 ?>
-<?php 
-    // Show title first on mobile
-        if (get_field( 'table_content' )) {
-    ?>
-        <h1 class="onTop"><?php the_title();?></h1>
-    <?php
-    }
-    ?>
 <div id="content-area" class="wrapper<?php if (get_field( 'table_content' )) {echo " tocActive";}?>">
-
-   <?php // table_content ( value )
-      if (get_field( 'table_content' )):
-      ?>
-      <aside id="stickyMenu" class="open">
-         <div>
-               <p>Sommaire</p>
-               <nav role="sommaire">
-                  <ol id="tocList">
-                     
-                  </ol>
-               </nav>
-         </div>
-      </aside>
-      <?php endif; ?>
+   <?php // table_content ( value ) 
+   if (get_field( 'table_content' )):
+      sedoo_labtools_display_sommaire('Sommaire');
+   endif; ?>
    <main id="main" class="site-main">
-      
       <div class="wrapper-content">
-      
       <?php
-      // sedoo_labtools_show_categories($themes, $themeSlugRewrite);
-      include( get_template_directory() . '/template-parts/content-page.php' );
-
-
-      ?>
+         // sedoo_labtools_show_categories($themes, $themeSlugRewrite);
+         include( get_template_directory() . '/template-parts/content-page.php' );
+         ?>
 		</div>
-	</main><!-- #main -->
-   
-</div><!-- #primary -->
+	</main>
+</div>
+
 <?php
-endwhile; // End of the loop.
-// get_sidebar();
+endwhile; 
 get_footer();
