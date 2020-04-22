369a716cb3cedb4f9f15ab7bd6373189186b0e58
Pierre Vert <pierr65@gmail.com>
2020-04-22 17:16:48 +0200

    move function show TOC in wpth_sedoo_labs

--- single-sedoo-axe.php
+++ single-sedoo-axe.php
@@ -15,17 +15,19 @@ $axeTagSlugRewrite = "sedoo-axe-tag";
 ?>
 <!-- L'AFFICHAGE COMMENCE ICI -->
 <?php
-sedoo_labtools_test_if_post_thumbnail_and_display();
+if (function_exists('sedoo_wpth_labs_test_if_post_thumbnail_and_display')) {
+sedoo_wpth_labs_test_if_post_thumbnail_and_display();
+}
 // Show title first on mobile
-if (get_field( 'table_content' )) {
-   sedoo_labtools_display_title_on_top_on_mobile();
+if ((get_field( 'table_content' )) && (function_exists('sedoo_wpth_labs_display_title_on_top_on_mobile'))) {
+   sedoo_wpth_labs_display_title_on_top_on_mobile();
 }
 ?>
 <div id="content-area" class="wrapper<?php if (get_field( 'table_content' )) {echo " tocActive";}?>">
    <?php // table_content ( value ) 
-   if (get_field( 'table_content' )):
-      sedoo_labtools_display_sommaire('Sommaire');
-   endif; ?>
+   if ((get_field( 'table_content' )) && (function_exists('sedoo_wpth_labs_display_sommaire'))) {
+      sedoo_wpth_labs_display_sommaire('Sommaire');
+   } ?>
    <main id="main" class="site-main">
       
       <div class="wrapper-content">
